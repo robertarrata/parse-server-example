92305997d5c94fc1ab4c63bcc7a2911a7d1e27a4
rarrata <rarrata@app-ology.com>
2016-11-25 17:57:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
+var mailGun = require("parse-server-simple-mailgun-adapter");
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI;
 
